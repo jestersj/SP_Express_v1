d9ee02b55e59acbf3e4a6ea19b0faab0191a89bb
denisfeo <dfeoktistov523@icloud.com>
2022-08-24 11:25:57 +0300

    Login added

--- SP_Express_v1/Controllers/AccountController.cs
+++ SP_Express_v1/Controllers/AccountController.cs
@@ -11,6 +11,7 @@ namespace SP_Express_v1.Controllers
     public class AccountController : Controller
     {
         private readonly UserManager<IdentityUser> _userManager;
+
         private readonly SignInManager<IdentityUser> _signInManager;
         // private readonly IDbInsert _repo;
         // private readonly IEmailDbService _EmailService;
@@ -20,41 +21,50 @@ namespace SP_Express_v1.Controllers
             _userManager = userManager;
             _signInManager = signInManager;
         }
-        
+
         [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
-        
+
         [HttpPost]
         [AutoValidateAntiforgeryToken]
-        public IActionResult Login(LoginViewModel model)
+        public async Task<IActionResult> Login(LoginViewModel model)
         {
-            if (ModelState.IsValid)
-            {
-                
+            if (!ModelState.IsValid) return View(model);
+            
+            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
+            
+            if (result.Succeeded) {
+                return RedirectToAction("Index", "Home");
+            } else {
+                ModelState.AddModelError("", "Неверный логин/пароль");
+                return View(model);
             }
-            return View(model);
+            
         }
 
         public IActionResult Register()
         {
             return View();
         }
-        
+
+
+        #region Регистрация физ. лиц
+
         [HttpGet]
         public IActionResult SignInInd()
         {
             return View();
         }
-        
+
         [HttpPost]
         [AutoValidateAntiforgeryToken]
         public async Task<IActionResult> SignInInd(RegisterViewModel model)
         {
             if (!ModelState.IsValid) return View(model);
-            
+
             var resultUser = await _userManager.FindByEmailAsync(model.Email).ConfigureAwait(false);
 
             if (resultUser != null)
@@ -62,7 +72,7 @@ namespace SP_Express_v1.Controllers
                 ModelState.AddModelError(string.Empty, "Пользователь уже существует");
                 return View(model);
             }
-            
+
             IdentityUser user = new()
             {
                 UserName = model.Email,
@@ -71,25 +81,29 @@ namespace SP_Express_v1.Controllers
             };
 
             var result = await _userManager.CreateAsync(user, model.Password);
-            
+
             if (!result.Succeeded)
             {
                 foreach (var error in result.Errors)
                 {
                     ModelState.AddModelError(string.Empty, error.Description);
                 }
+
                 return View(model);
             }
-            
+
             await _userManager.AddToRoleAsync(user, "usr");
 
             return RedirectToAction("Login", "Account");
         }
-        
+
+        #endregion
+
         public IActionResult SignInLegal()
         {
             return View();
         }
+
         public IActionResult UserLk()
         {
             return View();
