21a91f4470e51530b4680b8a3823597e76f6b19e
Mikhail Valuev <valuevmp@yandex.ru>
2022-08-25 13:15:08 +0300

    Fixed weird checkbox and modified menus with exit buttons

--- SP_Express_v1/wwwroot/js/site.js
+++ SP_Express_v1/wwwroot/js/site.js
@@ -1,8 +1,7 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
-//Маска ввода
+/////Input mask for phone numbers
 window.addEventListener("DOMContentLoaded", function() {
     [].forEach.call( document.querySelectorAll('.tel'), function(input) {
     var keyCode;
@@ -39,7 +38,6 @@ window.addEventListener("DOMContentLoaded", function() {
   });
 
 });
-
 
 
 
